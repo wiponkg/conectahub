5aa9b6eaa4c328d126743bb152d895a22fdc7c8a
wiponkg <73044227+wiponkg@users.noreply.github.com>
2025-11-23 22:17:11 -0300

    feat: Update Firebase storage bucket URL
    Corrects the `storageBucket` value in the Firebase configuration to the proper `appspot.com` domain.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,23 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Configuration provided from Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyArb_39bzejeoOhx6ZMRXSVRGhUOlN1sGs",
   authDomain: "conectahub-bfd8c.firebaseapp.com",
   projectId: "conectahub-bfd8c",
-  storageBucket: "conectahub-bfd8c.firebasestorage.app",
+  storageBucket: "conectahub-bfd8c.appspot.com",
   messagingSenderId: "714783587280",
   appId: "1:714783587280:web:a92fae765cd8d1960b97d5",
   measurementId: "G-R7SM6734M6"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta os serviços de Autenticação e Banco de Dados para uso no app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-export default app;
+export default app;
